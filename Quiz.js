14f644cd7e85c27cd8c0142e7a1c8503f0e1e54e
Kthal21 <71498624+Kthal21@users.noreply.github.com>
2021-04-12 10:43:59 +0530

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,52 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+
+    question.hide ();
+Contestant.getContestantInfo ();
+    //write code to change the background color here
+
+    //write code to show a heading for showing the result of Quiz
+textSize (21)
+text("Results!",200,50)
+    //call getContestantInfo( ) here
+
+
+    //write condition to check if contestantInfor is not undefined
+
+    //write code to add a note here
+
+    //write code to highlight contest who answered correctly
+    
+  }
+
+}
